cd325cca8cf1875f45dcd71a03ba8b212b93675b
Lenti teszt <teszt@vagyok.hu>
2021-04-07 20:09:16 +0200

    7,8 feladat hozzáadása (Gergő)

--- kecske.js
+++ kecske.js
@@ -4,17 +4,22 @@ function $(nev){
     return document.querySelectorAll(nev);
 }
 
+function ID(nev) {
+    return document.getElementById(nev);
+}
+
 var kep=["kepek/kecske2.png","kepek/kaposzta.png","kepek/farkas.png"];
 
 function init(){
     for (var i = 0; i < $("aside p img").length; i++) {
-        $("aside p img")[i].addEventListener("mouseover",feladat5)
-        $("aside p img")[i].addEventListener("mouseout",feladat6)
+        $("aside p img")[i].addEventListener("mouseover",feladat5);
+        $("aside p img")[i].addEventListener("mouseout",feladat6);
     }
 
-    $("footer p")[0].innerHTML = "Váradi Ákos";
+    $("footer p")[0].innerHTML = "Váradi Ákos, Simon Gergely";
     for (var i = 0; i < $("#bal img").length; i++) {
         $("#bal img")[i].addEventListener("click",feladat3b);
+        $("#bal img")[i].addEventListener("click",feladat7);
 
     }
 }
@@ -35,3 +40,19 @@ function feladat6(){
     
 }
 
+
+function feladat7() {
+    var img = document.createElement("img");
+    img.src = kep[this.id - 1];
+    this.style.display = "none";
+    ID("csonak").appendChild(img);
+    ellenorzes(this.id);
+}
+var csonak = [];
+function ellenorzes(id) {
+    csonak += id;
+    console.log(id);
+    if (csonak.includes(1) && csonak.includes(3) || csonak.includes(1) && csonak.includes(2)) {
+        alert("Helytelen választás");
+    }
+}
